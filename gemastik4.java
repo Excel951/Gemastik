2c77281334c7bb7eb1265c0fb7029da5201edbac
Excel951 <abigailexcelsisdeo@gmail.com>
2022-09-15 15:46:13 +0700

    Pengerjaan Gemastik (15/09/22)

---
+++ gemastik4.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class gemastik4 {
+    public static void main(String[] args) {
+        // n = petarung
+        // petarung i memiliki kekuatan Pi
+
+        // petarung kekuatan lebih tinggi yang menang
+        // kekuatan dari pemenang menjadi abs(Pi - Pj)
+
+        // jika kekuatan sama, petarung index lebih kecil yang menang
+        int a = 0;
+        int b = -4;
+        int c = a - b;
+        System.out.println();
+    }
+}
